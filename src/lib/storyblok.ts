677d7d77f3ec24cdf6571301bdb788f1aff28a7d
DriesBos <info@driesbos.com>
2025-07-13 17:01:39 +0100

    deploy fix

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -4,7 +4,7 @@ import Page from '@/components/storyblok/page';
 import PagePlay from '@/components/storyblok/pageplay';
 
 export const getStoryblokApi = storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN || 'demo-token',
   components: {
     page: Page,
     pageplay: PagePlay,
