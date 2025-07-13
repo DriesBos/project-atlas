cae31f8458a35252fec92525aaa27f8b4ad95e06
DriesBos <info@driesbos.com>
2025-07-13 17:21:49 +0100

    form success redirect

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -13,7 +13,7 @@ export function Subscribe() {
   };
 
   return (
-    <form name="feedback" onSubmit={handleFormSubmit}>
+    <form name="feedback" action="/success" onSubmit={handleFormSubmit}>
       <input type="hidden" name="form-name" value="feedback" />
       <input name="name" type="text" placeholder="Name" required />
       <input name="email" type="text" placeholder="Email (optional)" />
