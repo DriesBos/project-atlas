dbad51c5a96b4e3dfa77b7ba92fdf4101e141681
DriesBos <info@driesbos.com>
2025-07-20 15:11:05 +0100

    added storyblok component

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,12 +1,16 @@
-import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
+import { apiPlugin, storyblokInit, SbReactComponentsMap } from '@storyblok/react/rsc';
 
 import Page from '@/components/storyblok/page';
+import SectionIntro from '@/components/storyblok/section-intro';
+
+const components: SbReactComponentsMap = {
+  page: Page,
+  'section-intro': SectionIntro,
+};
 
 export const getStoryblokApi = storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN || 'demo-token',
-  components: {
-    page: Page,
-  },
+  components,
   use: [apiPlugin],
   apiOptions: {
     region: 'us', // Make sure this matches your Storyblok space region
