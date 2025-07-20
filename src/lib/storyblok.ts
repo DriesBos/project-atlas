170f5485e9d1160df31307318af07db1a0fa8f47
DriesBos <info@driesbos.com>
2025-07-20 14:34:02 +0100

    storyblok: improved fetch + set proper region

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,16 +1,14 @@
 import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
 
 import Page from '@/components/storyblok/page';
-import PagePlay from '@/components/storyblok/pageplay';
 
 export const getStoryblokApi = storyblokInit({
   accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN || 'demo-token',
   components: {
     page: Page,
-    pageplay: PagePlay,
   },
   use: [apiPlugin],
   apiOptions: {
-    region: 'eu',
+    region: 'us', // Make sure this matches your Storyblok space region
   },
 });
--- src/utils/fetchStory.ts
+++ src/utils/fetchStory.ts
@@ -1,24 +1,21 @@
-import { ISbResponse } from '@storyblok/react/rsc';
 import { getStoryblokApi } from '@/lib/storyblok';
 
 export const fetchStory = async (
   version: 'draft' | 'published',
   slug?: string[]
 ) => {
-  getStoryblokApi();
-  const correctSlug = `/${slug ? slug.join('/') : 'home'}`;
+  const storyblokApi = getStoryblokApi();
+  const correctSlug = `${slug ? slug.join('/') : 'home'}`;
   console.log('FETCHSTORY', correctSlug, version);
 
-  return fetch(
-    `
-    https://api.storyblok.com/v2/cdn/stories${correctSlug}?version=${version}&token=${
-      version === 'published'
-        ? process.env.NEXT_PUBLIC_STORYBLOK_TOKEN
-        : process.env.NEXT_PREVIEW_STORYBLOK_TOKEN
-    }`,
-    {
-      next: { tags: ['cms'] },
-      cache: version === 'published' ? 'default' : 'no-store',
-    }
-  ).then((res) => res.json()) as Promise<{ story: ISbResponse }>;
+  try {
+    const { data } = await storyblokApi.get(`cdn/stories/${correctSlug}`, {
+      version: version,
+    });
+
+    return { story: data.story };
+  } catch (error) {
+    console.error('Error fetching story:', error);
+    throw error;
+  }
 };
