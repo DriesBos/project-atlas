bf4553fcd7905d28c5038062d6ba691fd5162626
DriesBos <info@driesbos.com>
2025-07-22 16:48:44 +0100

    optimise image loading

--- src/components/storyblok/sectionlanding/sectionlanding.tsx
+++ src/components/storyblok/sectionlanding/sectionlanding.tsx
@@ -94,7 +94,7 @@ const SectionLanding: React.FunctionComponent<SectionLandingProps> = ({
                 sizes="100vw"
                 quality={85}
                 priority={index === 0} // Only prioritize the first image
-                loading="lazy"
+                loading={index === 0 ? 'eager' : 'lazy'}
                 placeholder="blur"
                 blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
                 className={index === currentImageIndex ? 'active' : ''}
