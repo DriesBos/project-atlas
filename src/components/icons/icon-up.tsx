65026b620d17b053fc57e8b7778860b47e24e042
DriesBos <info@driesbos.com>
2025-07-23 14:07:42 +0100

    fix syntax

--- src/components/icons/icon-up.tsx
+++ src/components/icons/icon-up.tsx
@@ -1,7 +1,7 @@
 export default function IconUp() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 494 494">
-      <g clip-path="url(#a)">
+      <g clipPath="url(#a)">
         <path
           fill="currentColor"
           d="M257 494h-20V136h20v358ZM247.433.74l246.556 229.917-13.639 14.628L246.69 27.394 13.686 244.675.046 230.048 245.95.739 246.64 0l.05.048.053-.048.69.74Z"
