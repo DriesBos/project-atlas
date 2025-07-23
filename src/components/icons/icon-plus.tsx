8c1b503364e86b15b2926a255384be0eeef9bde4
DriesBos <info@driesbos.com>
2025-07-23 12:40:06 +0100

    icons update

--- src/components/icons/icon-plus.tsx
+++ src/components/icons/icon-plus.tsx
@@ -3,7 +3,7 @@ export default function IconPlus() {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 494 494">
       <path
         fill="currentColor"
-        d="M262 494h-30V287h30v207Zm232-232H0v-30h494v30Zm-232-55h-30V0h30v207Z"
+        d="M257 494h-20V297h20v197Zm237-237H0v-20h494v20Zm-237-60h-20V0h20v197Z"
       />
     </svg>
   );
--- src/components/icons/icon-up.tsx
+++ src/components/icons/icon-up.tsx
@@ -1,10 +1,17 @@
 export default function IconUp() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 491 493">
-      <path
-        fill="currentColor"
-        d="m212.05 27.407-.173-.156L238.126 3.14 241.5 0l29.531 26.698-.404.377 219.846 197.199L463.707 254 241.67 54.078 27.28 254 0 224.745 212.05 27.407ZM265 122v371h-40V122h40Z"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 494 494">
+      <g clip-path="url(#a)">
+        <path
+          fill="currentColor"
+          d="M257 494h-20V136h20v358ZM247.433.74l246.556 229.917-13.639 14.628L246.69 27.394 13.686 244.675.046 230.048 245.95.739 246.64 0l.05.048.053-.048.69.74Z"
+        />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="currentColor" d="M0 0h494v494H0z" />
+        </clipPath>
+      </defs>
     </svg>
   );
 }
